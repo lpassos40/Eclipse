f3d8681d6c5efcae79663261885a2df7662fa42b
Lucas Passos <72843756+lpassos40@users.noreply.github.com>
2022-09-28 20:45:59 -0300

    Add files via upload

---
+++ sla.js
@@ -0,0 +1,25 @@
+
+function azul (){
+    let sol = document.querySelector("div.sun") 
+
+    sol.style.backgroundColor ="#0198cd"
+    sol.style.borderColor = "#0198cd"
+}
+function amarelo (){
+    let sol = document.querySelector("div.sun") 
+
+    sol.style.backgroundColor ="#fcd25b"
+    sol.style.borderColor = "#fcd25b"
+}
+function verde (){
+    let sol = document.querySelector("div.sun") 
+
+    sol.style.backgroundColor ="#0ecaa1"
+    sol.style.borderColor = "#0ecaa1"
+}
+function red (){
+    let sol = document.querySelector("div.sun") 
+
+    sol.style.backgroundColor ="#ca0e0e"
+    sol.style.borderColor = "#ca0e0e"
+}
